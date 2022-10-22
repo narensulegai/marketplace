83ed5750996dc6e9b891c8c811e31eb18f04786a
Akash Kuratkar <akashkuratkar@gmail.com>
2022-10-22 16:31:03 -0700

    Chat Service initial commit

--- Frontend/src/components/company/Dashboard.js
+++ Frontend/src/components/company/Dashboard.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import Paginate from '../Paginate';
+import React from "react";
+import Paginate from "../Paginate";
+import { useHistory } from "react-router";
 
-const Dashboard = () => {
+const Dashboard = (props) => {
+  const history = useHistory();
+  const handleChatClick = () => {
+    history.push("/chat");
+  };
   return (
     <div className="row">
       <div className="col-12">
@@ -25,7 +30,9 @@ const Dashboard = () => {
               <th scope="row">1342</th>
               <td>Mark</td>
               <td>Otto</td>
-              <td><a href="mailto:mark@gmail.com">mark@gmail.com</a></td>
+              <td>
+                <a href="mailto:mark@gmail.com">mark@gmail.com</a>
+              </td>
               <td>
                 <div className="badge badge-pill badge-success">Progress</div>
               </td>
@@ -33,16 +40,25 @@ const Dashboard = () => {
                 <div className="small">15th Jan 2022, 5:20PM</div>
               </td>
               <td>
-                <button className="btn-primary">Chat</button>
+                <button
+                  className="btn-primary"
+                  onClick={() => handleChatClick()}
+                >
+                  Chat
+                </button>
               </td>
             </tr>
             <tr>
               <th scope="row">2231</th>
               <td>Jacob</td>
               <td>Thornton</td>
-              <td><a href="mailto:jacob@gmail.com">jacob@gmail.com</a></td>
               <td>
-                <div className="badge badge-pill badge-secondary">Submitted</div>
+                <a href="mailto:jacob@gmail.com">jacob@gmail.com</a>
+              </td>
+              <td>
+                <div className="badge badge-pill badge-secondary">
+                  Submitted
+                </div>
               </td>
               <td>
                 <div className="small">15th Jan 2022, 5:30PM</div>
@@ -55,7 +71,9 @@ const Dashboard = () => {
               <th scope="row">1233</th>
               <td>Larry</td>
               <td>Bird</td>
-              <td><a href="mailto:larry@gmail.com">larry@gmail.com</a></td>
+              <td>
+                <a href="mailto:larry@gmail.com">larry@gmail.com</a>
+              </td>
               <td>
                 <div className="badge badge-pill badge-success">Progress</div>
               </td>
@@ -70,7 +88,9 @@ const Dashboard = () => {
               <th scope="row">1232</th>
               <td>Guy</td>
               <td>Larry</td>
-              <td><a href="mailto:guy@gmail.com">guy@gmail.com</a></td>
+              <td>
+                <a href="mailto:guy@gmail.com">guy@gmail.com</a>
+              </td>
               <td>
                 <div className="badge badge-pill badge-danger">Denied</div>
               </td>
@@ -85,7 +105,9 @@ const Dashboard = () => {
               <th scope="row">1212</th>
               <td>Thrump</td>
               <td>Joe</td>
-              <td><a href="mailto:thrump@gmail.com">thrump@gmail.com</a></td>
+              <td>
+                <a href="mailto:thrump@gmail.com">thrump@gmail.com</a>
+              </td>
               <td>
                 <div className="badge badge-pill badge-danger">Denied</div>
               </td>
@@ -100,12 +122,7 @@ const Dashboard = () => {
         </table>
       </div>
       <div className="col-12">
-        <Paginate
-          numItems={10}
-          onPageChange={() => {
-          }}
-          currentPage={0}
-                />
+        <Paginate numItems={10} onPageChange={() => {}} currentPage={0} />
       </div>
     </div>
   );
